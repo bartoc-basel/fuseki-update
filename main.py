fccc1c587a115e5962315c93036d03cefee762da
bartoc-basel <bartoc.skosmos@gmail.com>
2018-03-08 09:16:57 +0100

    Removed print message

--- main.py
+++ main.py
@@ -263,7 +263,6 @@ sheet.load_sheet(sheet_options['range'])
 
 count = 1
 for val in sheet.values:
-    print(sheet.values)
     update = SheetUpdate()
     if len(val) == int(sheet_options['sheet_length']):
         try:
