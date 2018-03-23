3701a37186737e0cbbeefdc7728cba80f0e2e9f3
Kordishal <jonaswaeber@gmail.com>
2018-03-23 08:33:30 +0100

    Added logging to construct_aat.py

--- construct_aat.py
+++ construct_aat.py
@@ -12,31 +12,34 @@ import zipfile
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-
-
 ontology = 'http://vocab.getty.edu/ontology.rdf'
-
+ontology_path = 'vocabularies/getty/base_ontology.ttl'
 aat_full = 'http://vocab.getty.edu/dataset/aat/full.zip'
 
 g = Graph()
+
+logging.info('Load base ontology for getty!')
 g.parse(ontology, format='xml')
+logging.info('Finished download and parsing of %s. Begin serialization.', ontology)
+g.serialize(ontology_path, format='ttl')
+logging.info('Serialized base ontology and saved in %s.', ontology_path)
 
-g.serialize('vocabularies/getty/base_ontology.ttl', format='ttl')
-
-
+logging.info('Download full aat zip!')
 response = requests.get(aat_full)
-
 if response.ok:
+    logging.info('Successfully downloaded aat!')
     files = list()
     buffer = io.BytesIO(response.content)
     z = zipfile.ZipFile(buffer)
     for n, i in zip(z.namelist(), z.infolist()):
+        logging.info('Uncompressed %s.', n)
         tmp = z.read(i).decode('utf-8')
         with open('vocabularies/getty/' + str(n), 'w') as file:
             file.write(tmp)
+            logging.info('Written %s to vocabularies/getty/.', n)
 
 
-
+logging.info('Finished Application')
 
 aat = Graph()
 aat.parse('vocabularies/getty/base_ontology.ttl')
