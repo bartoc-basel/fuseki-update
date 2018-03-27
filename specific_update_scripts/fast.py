a553385fc3a3a04d579be53b0204933f957e1d19
Kordishal <jonaswaeber@gmail.com>
2018-03-27 12:10:53 +0200

    Fixed fast download

--- specific_update_scripts/fast.py
+++ specific_update_scripts/fast.py
@@ -63,15 +63,19 @@ def update(config):
         with open(path + file_name, 'w', encoding='utf-8') as file:
             file.write(text)
 
+            logger.info('Downloaded and saved file.')
+
         SCHEMA = Namespace('http://schema.org/')
 
         g = Graph()
 
-        g.parse(path + file_name)
+        g.parse(path + file_name, format='n3')
         add_type(g, SCHEMA.Event, SKOS.Concept)
         add_skos_predicate_variant(g, RDFS.label, SKOS.prefLabel)
 
         file_name = file_name.replace('.n3', '.ttl')
         g.serialize(destination=path + file_name, format='ttl')
 
+        logger.info('Refactored graph with prefLabels & Concepts.')
         put_graph(graph, open(path + file_name))
+        logger.info('Uploaded graph to Fuseki.')
