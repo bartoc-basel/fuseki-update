99504006c33306e79cc34e2db7eca5aca2b565de
Kordishal <jonaswaeber@gmail.com>
2018-05-15 14:55:16 +0200

    Added option to log to stdout

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ parser.add_argument('--config', action='store', type=str, dest='voc_config', def
                     help='The config file used for this vocabulary in skosify. NYI')
 parser.add_argument('-all', action='store_true', dest='run_update',
                     help='Run the main update script to update the tripple store from the google spreadsheet.')
-parser.add_argument('-specific', action='store', dest='name', default=None)
+parser.add_argument('-s', action='store', dest='name', default=None)
 
 parser.add_argument('-delete', dest='delete_request', action='store_true')
 parser.add_argument('-put', dest='put_request', action='store_true')
@@ -25,38 +25,47 @@ parser.add_argument('--uri', nargs='?', default='')
 parser.add_argument('--url', nargs='?', default='')
 parser.add_argument('-t', dest='test_skosify', action='store_true')
 parser.add_argument('-l', dest='label', action='store')
-parser.add_argument('-d', dest='download', action='store_true', help='Download the file and do not just load it from disc. Default False.')
+parser.add_argument('-k', dest='download', action='store_true', help='Download the file and do not just load it from '
+                                                                     'disc. Default False.')
 parser.add_argument('--namespace', action='store', default=None)
 parser.add_argument('--default-language', action='store', default=None)
+
+parser.add_argument('-d', dest='debug', action='store_true', help='Ignore default logging configuration and simply '
+                                                                  'log to stdout.')
 
 args = parser.parse_args()
 
 config = ConfigParser(interpolation=None)
 config.read(args.config)
 
-# options for what should be logged.
-logging_options = dict()
-for key, val in config.items('logger'):
-    logging_options[key] = val
 
-logging_options['filename'] = config['data']['base'] + config['data']['output'] + logging_options['filename']
-
-
-debug_levels = {
-    'debug': logging.DEBUG,
-    'info': logging.INFO,
-    'warning': logging.WARNING,
-    'error': logging.ERROR,
-    'critical': logging.CRITICAL
-}
-
-# tanslate logging level.
-if 'level' in logging_options:
-    logging_options['level'] = debug_levels[logging_options['level']]
+if args.debug:
+    import sys
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 else:
-    logging_options['level'] = logging.CRITICAL
+    # options for what should be logged.
+    logging_options = dict()
+    for key, val in config.items('logger'):
+        logging_options[key] = val
 
-logging.basicConfig(**logging_options)
+    logging_options['filename'] = config['data']['base'] + config['data']['output'] + logging_options['filename']
+
+
+    debug_levels = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+
+    # tanslate logging level.
+    if 'level' in logging_options:
+        logging_options['level'] = debug_levels[logging_options['level']]
+    else:
+        logging_options['level'] = logging.CRITICAL
+
+    logging.basicConfig(**logging_options)
 
 data_path = config['data']['base']
 logging.debug('Base path: ' + data_path)
